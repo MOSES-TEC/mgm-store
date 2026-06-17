582546db08e7608bda540de4f9eb959aa9e26ac0
MOSES Y <ymoses546@gmail.com>
2026-06-17 23:25:35 +0530

    feat: update dependencies and integrate Sentry for error tracking
    - Added new dependencies: @sentry/react, @tailwindcss/vite, @tanstack/react-query, lucide-react, and react-router.
    
    - Created new components: Footer, Layout, Navbar, PageLoader, SentryErrorFallback, and SentryUserSync for better structure and error handling.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import "./App.css";
-import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import { Show, SignInButton, SignUpButton, useAuth, UserButton } from "@clerk/react";
+import PageLoader from "./components/PageLoader";
+import Layout from "./components/Layout";
 
 
 function App() {
 
+  const { isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) return <PageLoader />;
+
   return (
-    <>
+    <Layout>
       <header>
         <Show when="signed-out">
           <SignInButton mode="modal" />
@@ -15,8 +20,9 @@ function App() {
           <UserButton />
         </Show>
       </header>
-    </>
-  )
-}
 
-export default App
+    </Layout>
+  );
+};
+
+export default App;
